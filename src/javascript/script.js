b094687c5e9f8740d5c67f427d222c7142d8d504
eulerfernandes <pedraeuler@gmail.com>
2024-08-20 23:03:53 -0300

    adding javascript

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("#mobile_btn").on("click", function () {
-    $("#mobile_menu").toggleclass("active");
-    $("#mobile_btn").find("i").toggleclass("fa-x");
+    $("#mobile_menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
   });
 });
